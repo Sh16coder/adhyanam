de1973cb9432f9897659ee268adff2a729907bae
Sh16coder <158461477+Sh16coder@users.noreply.github.com>
2025-12-16 21:44:29 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // Replace with your Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCJoAlb3v82-Y_alzd9Wl73Y4O1QPF2hDM",
+  authDomain: "adhyanam-511eb.firebaseapp.com",
+  projectId: "adhyanam-511eb",
+  storageBucket: "adhyanam-511eb.firebasestorage.app",
+  messagingSenderId: "242165230864",
+  appId: "1:242165230864:web:aecb28689cff0895718995"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
